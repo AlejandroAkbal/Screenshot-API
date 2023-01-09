aa5242d144291fc93088ff67cea1759f412573fe
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-01-09 17:36:34 +0100

    feat: use fastify adapter

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
+
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(
+    AppModule,
+    new FastifyAdapter()
+  );
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
