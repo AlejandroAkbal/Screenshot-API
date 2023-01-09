d5b6070530c4b349f87e6760b1ca1441f604d08e
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-01-09 18:05:51 +0100

    feat: enable compression

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import {NestFactory} from '@nestjs/core';
 import {FastifyAdapter} from '@nestjs/platform-fastify';
 import {AppModule} from './app.module';
 import {VersioningType} from "@nestjs/common";
+import compression from '@fastify/compress'
 
 async function bootstrap() {
     const app = await NestFactory.create(
@@ -12,6 +13,8 @@ async function bootstrap() {
     app.enableVersioning({
         type: VersioningType.URI,
     });
+
+    await app.register(compression, {encodings: ['gzip', 'deflate']});
 
     await app.listen(3000, '0.0.0.0');
 }
