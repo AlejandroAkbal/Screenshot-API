94352ac8b890f637976b6ed1e787565de60d349b
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-01-10 15:14:10 +0100

    feat: allow to be run in docker

--- src/app.controller.js
+++ src/app.controller.js
@@ -1,49 +1,74 @@
-import {Bind, Controller, Dependencies, Get, Header, Query, StreamableFile, ValidationPipe} from '@nestjs/common';
-import {AppService} from './app.service';
-import {CaptureDTO} from "./dto/CaptureDTO";
+import {
+  Bind,
+  Controller,
+  Dependencies,
+  Get,
+  Header,
+  Query,
+  StreamableFile,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AppService } from './app.service';
+import { CaptureDTO } from './dto/CaptureDTO';
 
 @Controller({
-    version: '1',
+  version: '1',
 })
 @Dependencies(AppService)
 export class AppController {
-    constructor(appService) {
-        this.appService = appService;
-    }
+  constructor(appService) {
+    this.appService = appService;
+  }
 
-    @Get('capture')
-    @Bind(Query(new ValidationPipe({
+  @Get('capture')
+  @Bind(
+    Query(
+      new ValidationPipe({
         transform: true,
         whitelist: true,
         forbidNonWhitelisted: true,
-        expectedType: CaptureDTO
-    })))
-    // Cache 1 hour on CDN | Cache 30 minutes on client | Stale while revalidate 30 minutes
-    @Header('Cache-Control', 'max-age=1800, s-maxage=3600, stale-while-revalidate=1800')
-    async getCapture(query) {
-        /**
-         * @type {import('capture-website')}
-         */
-        const {default: captureWebsite} = await (eval(`import('capture-website')`));
+        expectedType: CaptureDTO,
+      }),
+    ),
+  )
+  // Cache 1 hour on CDN | Cache 30 minutes on client | Stale while revalidate 30 minutes
+  @Header(
+    'Cache-Control',
+    'max-age=1800, s-maxage=3600, stale-while-revalidate=1800',
+  )
+  async getCapture(query) {
+    /**
+     * @type {import('capture-website')}
+     */
+    const { default: captureWebsite } = await eval(`import('capture-website')`);
 
-        const websiteBuffer = await captureWebsite.buffer(query.url, {
-            width: query.width,
-            height: query.height,
+    const websiteBuffer = await captureWebsite.buffer(
+      query.url,
+      {
+        width: query.width,
+        height: query.height,
 
-            type: query.mime_type,
-            quality: query.quality,
+        type: query.mime_type,
+        quality: query.quality,
 
-            timeout: 5, // In seconds
-            delay: query.delay,
+        timeout: 5, // In seconds
+        delay: query.delay,
 
-            disableAnimations: true,
-            blockAds: true,
-        });
+        disableAnimations: true,
+        blockAds: true,
+      },
+      {
+        launchOptions: {
+          // Insecure way to allow it running in Docker
+          args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        },
+      },
+    );
 
-        return new StreamableFile(websiteBuffer, {
-            disposition: 'inline',
-            type: `image/${query.mime_type}`,
-            length: websiteBuffer.length,
-        })
-    }
+    return new StreamableFile(websiteBuffer, {
+      disposition: 'inline',
+      type: `image/${query.mime_type}`,
+      length: websiteBuffer.length,
+    });
+  }
 }
