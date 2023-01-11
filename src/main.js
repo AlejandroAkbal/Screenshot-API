14b63d0eca3385e53bce64eb23777f81357f8eca
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-01-11 15:32:54 +0100

    feat: enable cors for everyone temporarily

--- src/main.js
+++ src/main.js
@@ -1,33 +1,35 @@
-import {NestFactory} from '@nestjs/core';
-import {FastifyAdapter} from '@nestjs/platform-fastify';
-import {AppModule} from './app.module';
-import {VersioningType} from "@nestjs/common";
+import { NestFactory } from '@nestjs/core'
+import { FastifyAdapter } from '@nestjs/platform-fastify'
+import { AppModule } from './app.module'
+import { VersioningType } from '@nestjs/common'
 import compression from '@fastify/compress'
 import helmet from '@fastify/helmet'
 
 async function bootstrap() {
-    const app = await NestFactory.create(
-        AppModule,
-        new FastifyAdapter()
-    );
+	const app = await NestFactory.create(AppModule, new FastifyAdapter())
 
-    app.enableVersioning({
-        type: VersioningType.URI,
-    });
+	app.enableVersioning({
+		type: VersioningType.URI
+	})
 
-    app.register(helmet)
-    await app.register(compression, {encodings: ['gzip', 'deflate']});
+	app.register(helmet)
+	await app.register(compression, { encodings: ['gzip', 'deflate'] })
 
-    // app.enableCors((req, callback) => {
-    //     const isDomainAllowed = ALLOWED_HOSTS.includes(req.hostname);
-    //
-    //     callback(null, {
-    //         origin: isDomainAllowed,
-    //         credentials: true,
-    //     });
-    // })
+	app.enableCors({
+		origin: true,
+		credentials: true
+	})
 
-    await app.listen(process.env.PORT || 3000, '0.0.0.0');
+	// app.enableCors((req, callback) => {
+	//     const isDomainAllowed = ALLOWED_HOSTS.includes(req.hostname);
+	//
+	//     callback(null, {
+	//         origin: isDomainAllowed,
+	//         credentials: true,
+	//     });
+	// })
+
+	await app.listen(process.env.PORT || 3000, '0.0.0.0')
 }
 
-bootstrap();
+bootstrap()
