5b381eefe0b03b350d81cf99fbe5f94e570ed4f7
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-01-11 15:50:02 +0100

    feat: use common user agent

--- src/app.controller.js
+++ src/app.controller.js
@@ -1,85 +1,76 @@
-import {
-  Bind,
-  Controller,
-  Dependencies,
-  Get,
-  Header,
-  Query,
-  StreamableFile,
-  ValidationPipe,
-} from '@nestjs/common';
-import { AppService } from './app.service';
-import { CaptureDTO } from './dto/CaptureDTO';
+import { Bind, Controller, Dependencies, Get, Header, Query, StreamableFile, ValidationPipe } from '@nestjs/common'
+import { AppService } from './app.service'
+import { CaptureDTO } from './dto/CaptureDTO'
 
 @Controller({
-  version: '1',
+	version: '1'
 })
 @Dependencies(AppService)
 export class AppController {
-  constructor(appService) {
-    this.appService = appService;
-  }
+	constructor(appService) {
+		this.appService = appService
+	}
 
-  @Get('status')
-  getStatus() {
-    return { status: 'ok' };
-  }
+	@Get('status')
+	getStatus() {
+		return { status: 'ok' }
+	}
 
-  @Get('capture')
-  @Bind(
-    Query(
-      new ValidationPipe({
-        transform: true,
-        whitelist: true,
-        forbidNonWhitelisted: true,
-        expectedType: CaptureDTO,
-      }),
-    ),
-  )
-  // Cache 1 hour on CDN | Cache 30 minutes on client | Stale while revalidate 30 minutes
-  @Header(
-    'Cache-Control',
-    'max-age=1800, s-maxage=3600, stale-while-revalidate=1800',
-  )
-  async getCapture(query) {
-    /**
-     * @type {import('capture-website')}
-     */
-    const { default: captureWebsite } = await eval(`import('capture-website')`);
+	@Get('capture')
+	@Bind(
+		Query(
+			new ValidationPipe({
+				transform: true,
+				whitelist: true,
+				forbidNonWhitelisted: true,
+				expectedType: CaptureDTO
+			})
+		)
+	)
+	// Cache 1 hour on CDN | Cache 30 minutes on client | Stale while revalidate 30 minutes
+	@Header('Cache-Control', 'max-age=1800, s-maxage=3600, stale-while-revalidate=1800')
+	async getCapture(query) {
+		/**
+		 * @type {import('capture-website')}
+		 */
+		const { default: captureWebsite } = await eval(`import('capture-website')`)
 
-    const websiteBuffer = await captureWebsite.buffer(query.url, {
-      width: query.width,
-      height: query.height,
+		const websiteBuffer = await captureWebsite.buffer(query.url, {
+			width: query.width,
+			height: query.height,
 
-      type: query.mime_type,
-      quality: query.quality,
+			type: query.mime_type,
+			quality: query.quality,
 
-      timeout: 5, // In seconds
-      delay: query.delay,
+			timeout: 5, // In seconds
+			delay: query.delay,
 
-      disableAnimations: true,
-      blockAds: true,
+			disableAnimations: true,
+			blockAds: true,
 
-      launchOptions: {
-        args: [
-          // Insecure way to allow it running in Docker
-          '--headless',
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-          '--disable-gpu',
+			userAgent:
+				'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
 
-          // Aesthetic
-          '--hide-scrollbars',
-          '--mute-audio',
-          '--use-fake-ui-for-media-stream', // Pages that ask for webcam/microphone access
-        ],
-      },
-    });
+			launchOptions: {
+				args: [
+					// Insecure way to allow it running in Docker
+					'--headless',
+					'--no-sandbox',
+					'--disable-setuid-sandbox',
+					'--disable-gpu',
 
-    return new StreamableFile(websiteBuffer, {
-      disposition: 'inline',
-      type: `image/${query.mime_type}`,
-      length: websiteBuffer.length,
-    });
-  }
+					// Aesthetic
+					'--hide-scrollbars',
+					'--mute-audio',
+					'--use-fake-ui-for-media-stream' // Pages that ask for webcam/microphone access
+				]
+			}
+		})
+
+		return new StreamableFile(websiteBuffer, {
+			disposition: 'inline',
+			type: `image/${query.mime_type}`,
+			length: websiteBuffer.length
+		})
+	}
 }
