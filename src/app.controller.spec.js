70dcb84e9af304e2cd4ef27a3f0a58d5fcfb02fb
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-01-19 00:00:58 +0100

    test: add simple tests

--- src/app.controller.spec.js
+++ src/app.controller.spec.js
@@ -1,22 +1,25 @@
-import { Test } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Test } from '@nestjs/testing'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
 
 describe('AppController', () => {
-  let appController;
+	let appController
 
-  beforeEach(async () => {
-    const app = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
+	beforeEach(async () => {
+		const app = await Test.createTestingModule({
+			controllers: [AppController],
+			providers: [AppService]
+		}).compile()
 
-    appController = app.get(AppController);
-  });
+		appController = app.get(AppController)
+	})
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
-  });
-});
+	describe('/v1/status', () => {
+		//
+
+		test('GET', () => {
+			expect(appController.getStatus()).toEqual({ status: 'ok' })
+		})
+	})
+
+})
